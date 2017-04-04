af5cbc7e7702624da1cbaede14da92d8a0fbbcde
jason.berk <jsaon.berk@nextgearcapital.com>
2017-04-04 15:28:30 -0400

    use docx4j which relies on log4j

--- src/main/java/com/isf/MovieController.java
+++ src/main/java/com/isf/MovieController.java
@@ -1,6 +1,9 @@
 package com.isf;
 
 import lombok.extern.slf4j.Slf4j;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +25,21 @@ public class MovieController {
     public Movie getMovies(@PathVariable String term){
         Movie movie = Movie.from(restTemplate.getForObject("http://www.omdbapi.com/?t=" + term, Map.class));
         log.debug("got back movies: {}", movie);
+
+        saveAsWord(movie);
+
         return movie;
+    }
+
+    private void saveAsWord(Movie movie) {
+        try {
+            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.createPackage();
+            MainDocumentPart mdp = wordMLPackage.getMainDocumentPart();
+            mdp.addParagraphOfText(new JSONObject(movie).toString(4));
+            wordMLPackage.save(new java.io.File(movie.getTitle() + ".docx") );
+        }catch (Exception e){
+            log.error("failed to create word doc from movie {}", movie);
+            throw new RuntimeException(e);
+        }
     }
 }

0309dea2be6b1a725c755dbe7b0958488e42cb62
jason.berk <jsaon.berk@nextgearcapital.com>
2017-04-04 14:51:41 -0400

    show how lambdas can be used with log4j2

--- src/main/java/com/isf/MovieController.java
+++ src/main/java/com/isf/MovieController.java
@@ -1,6 +1,6 @@
 package com.isf;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +8,11 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+// SLF4J doesn't support lambdas (yet)
+// https://github.com/qos-ch/slf4j/pull/70
+// to make this example work, we need to use log4j2 directly
+@Log4j2
 @RestController
-@Slf4j
 public class MovieController {
 
     private RestTemplate restTemplate;
@@ -22,6 +25,15 @@ public class MovieController {
     public Movie getMovies(@PathVariable String term){
         Movie movie = Movie.from(restTemplate.getForObject("http://www.omdbapi.com/?t=" + term, Map.class));
         log.debug("got back movies: {}", movie);
+
+        // if a method used in the logger is slow....
+        log.debug("not using a lambda: '{}'", someComplexMethod());
+        log.debug("using a lambda: '{}'", () -> someComplexMethod());
         return movie;
+    }
+
+    private String someComplexMethod() {
+        System.out.println("DOING SOMETHING LONG AND INTENSIVE");
+        return "a difficult to calculate string";
     }
 }

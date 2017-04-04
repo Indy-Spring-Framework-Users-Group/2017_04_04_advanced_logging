faf44bb72a6c2c43cf181049ffe5f00f709e1d09
jason.berk <jsaon.berk@nextgearcapital.com>
2017-04-04 18:02:25 -0400

    working

--- src/main/java/com/isf/LoggingApplication.java
+++ src/main/java/com/isf/LoggingApplication.java
@@ -10,6 +10,6 @@ public class LoggingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoggingApplication.class, args);
-	}
+    }
 
 }
--- src/main/java/com/isf/MovieController.java
+++ src/main/java/com/isf/MovieController.java
@@ -1,6 +1,8 @@
 package com.isf;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.slf4j.MarkerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,16 @@ public class MovieController {
 
     @GetMapping(value = "movie/{term}")
     public Movie getMovies(@PathVariable String term){
+        // assume these values were put into the MDC when the user authenticated
+        MDC.put("user.login", "jberk");
+        MDC.put("user.name", "Jason Berk");
+
         Movie movie = Movie.from(restTemplate.getForObject("http://www.omdbapi.com/?t=" + term, Map.class));
         log.info("got back movies: {}", movie);
+        if(movie.getRating().equalsIgnoreCase("R")) {
+            log.warn(MarkerFactory.getMarker("RATED_R"), "{}", movie);
+        }
+        MDC.clear();
         return movie;
     }
 }

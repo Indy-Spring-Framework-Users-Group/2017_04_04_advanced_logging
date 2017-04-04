f38d8d753be5748e34be5a7aabcfe41aef398cec
jason.berk <jsaon.berk@nextgearcapital.com>
2017-04-04 13:02:38 -0400

    simple-logging

---
+++ src/main/java/com/isf/MovieController.java
@@ -0,0 +1,27 @@
+package com.isf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class MovieController {
+
+    private RestTemplate restTemplate;
+
+    public MovieController(){
+        this.restTemplate = new RestTemplate();
+    }
+
+    @GetMapping(value = "movie/{term}")
+    public Movie getMovies(@PathVariable String term){
+        Movie movie = Movie.from(restTemplate.getForObject("http://www.omdbapi.com/?t=" + term, Map.class));
+        log.debug("got back movies: {}", movie);
+        return movie;
+    }
+}
